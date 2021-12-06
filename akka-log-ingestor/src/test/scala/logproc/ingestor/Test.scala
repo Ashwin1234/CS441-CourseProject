6d4a32843672a579d664aba6e2008f9d9c7f7afb
srthiru <srthiruvenkadam@gmail.com>
2021-12-06 01:16:53 -0600

    stat format change

--- akka-log-ingestor/src/test/scala/logproc/ingestor/Test.scala
+++ akka-log-ingestor/src/test/scala/logproc/ingestor/Test.scala
@@ -3,6 +3,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
+import com.google.gson.Gson
+
+import logproc.data._
+
+case class JsonStat(app: String, windowstart: Long, windowend: Long, numLogs: Long, numErrors: Long)
 
 // Test cases
 class Test extends AnyFlatSpec with Matchers {
@@ -11,16 +16,17 @@ class Test extends AnyFlatSpec with Matchers {
   val config: Config = ConfigFactory.load("application.conf")
 
 
-  it should "bucket name" in {
+  it should "have the correct bucket name" in {
     config.getString("s3.bucket") shouldBe "logs-project"
   }
-  it should "key name" in {
+  it should "have a valid key name" in {
     config.getString("s3.key") shouldBe "logs/new_log.log"
   }
-  it should "region name" in {
+  it should "have the correct region name" in {
     config.getString("s3.region") shouldBe "Regions.US_EAST_1"
   }
   it should "time" in {
+    val stat = JsVal()
     config.getString("kafka.time") shouldBe "1489997145000L"
   }
   it should "window" in {
